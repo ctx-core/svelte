3ea888ec55bb44266ba03ffec6d07f39c4228351
Brian Takita <brian.takita@gmail.com>
2020-01-02 03:28:01 -0500

    feat: lib: llContext,_store_ctx__llContext

---
+++ lib.d.ts
@@ -0,0 +1,2 @@
+export declare function llContext(key: any, _val: any): any;
+export declare const _store_ctx__llContext: () => any;
---
+++ lib.ts
@@ -0,0 +1,10 @@
+import { getContext, setContext } from 'svelte'
+export function llContext(key, _val) {
+	let val = getContext(key)
+	if (val != null) return val
+	val = _val()
+	setContext(key, val)
+	return val
+}
+export const _store_ctx__llContext = ()=>
+	llContext('store_ctx', ()=>({}))
