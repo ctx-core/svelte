d9a9995c398594c041e223e17516e01082303835
Brian Takita <brian.takita@gmail.com>
2020-08-05 13:33:25 -0700

    ts outputs commonjs

--- lib.d.ts
+++ lib.d.ts
@@ -1,3 +1,3 @@
-export declare function llContext(key: any, _val: () => any): any;
-export declare const _store_ctx__llContext: () => any;
+export declare function llContext(key: any, _val: () => any): unknown;
+export declare const _store_ctx__llContext: () => unknown;
 export declare type EventDispatcher = (type: string, detail?: any) => void;
