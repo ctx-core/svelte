92d308033ede1e2c646b712e84c823025c1d98e4
Brian Takita <brian.takita@gmail.com>
2022-08-31 13:27:54 -0500

    subscribe_wait_timeout: - unused import

--- src/store/subscribe_wait_timeout/index.js
+++ src/store/subscribe_wait_timeout/index.js
@@ -1,4 +1,3 @@
-import { promise_timeout } from '@ctx-core/function'
 import { subscribe_wait } from '../subscribe_wait/index.js'
 export function subscribe_wait_timeout(store, condition_fn, timeout) {
 	return subscribe_wait(store, condition_fn, timeout)
